619edd3b3c8ed279042a4b4d1e4a95d3b3fcf0ea
Oscar Shoudt <shoudto@gmail.com>
2019-09-11 16:06:49 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+require 'pry'
+require 'prime'
+
+#in pry =>argument(number) = 2
+
+def prime?(number)
+  # binding.pry
+  return false if number < 2
+  (2..number - 1 ).each do|num|
+    if (number % num) == 0
+    return false
+    end
+  end
+    true
+end 
